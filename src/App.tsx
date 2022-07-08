1a7970fda4be9950341a6ebf2401bd2a6f6d61f1
Satyabrata Das <satyabrata_pw1_135@masai.school>
2022-07-08 16:35:14 +0530

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -5,48 +5,10 @@ import { useState,useEffect } from 'react';
 import Cards from './Components/Cards'
 import {Routing} from './Router/Router'
 
-// export type CartItemTypes = {
-//   id: number;
-//   category: string;
-//   description: string;
-//   image: string;
-//   price: number;
-//   title: string;
-//   amount: number;
-// };
-
-// function IsLoading(){
-//   return (
-//     <div className="spiner-div"><div className="spinner-container">
-//     <div className="loading-spinner">
-//     </div>
-//   </div></div>
-//   )
-// }
-
 
 function App() {
 
  
-  // const [data, setData] = useState<CartItemTypes[]>([]);
-  // const [looding,setLooding] = useState<boolean>(true);
-  // useEffect(() => {
-    
-  //   const fetchProducts = async ()=> {
-  //   //  setLooding(true)
-  //     const data = await fetch("https://fakestoreapi.com/products");
-  //     const result = await data.json();
-      
-  //     setData(result);
-  //     setLooding(false)
-  //   };
-  //   fetchProducts()
-  // },[data])
-
-  // const handleAddToCart = (clickedItem: CartItemTypes) => null;
-
-
-
 
 
   return (
@@ -54,14 +16,7 @@ function App() {
      <Navbar
       cartvelue={5}
      />
-     {/* <div className="text">
-      {looding ?  < IsLoading/> : data.map((item) => (
-         
-         <Cards items={item} handleAddToCart={handleAddToCart} />
-      
-     )) }
-     
-     </div> */}
+ 
      <Routing/>
     </div>
   );
